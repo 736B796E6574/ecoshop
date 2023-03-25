452fe48fbc8d675edb5b72fc96665a7a3e79ea10
martin-mcinerney <martin_mcinerney@live.ie>
2023-03-25 18:35:36 +0000

    checkout routing

--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,17 @@ import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
 import "./Header.css";
+import { Link } from "react-router-dom"
 
 export default function Header() {
   return (
     <div className='header'>
+        <Link to="/" style={{ textDecoration: "none" }}>
         <div className="header__logo">
       <StorefrontIcon className='header__logoImage' fontSize='large' />
       <h2 className="header__logoTitle">EcoShop</h2>
       </div>
+      </Link>
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className='header__searchIcon' />
@@ -26,10 +29,12 @@ export default function Header() {
           <span className="nav__itemLineOne">Your</span>
           <span className="nav__itemLineTwo">Shop</span>
         </div>
+        <Link to="/checkout" style={{ textDecoration: "none" }}>
         <div className='nav__itemBasket'>
           <ShoppingBasketIcon  />
           <span className="nav__itemLineTwo nav__basketCount">0</span>
         </div>
+        </Link>
         
       </div>
     </div>
